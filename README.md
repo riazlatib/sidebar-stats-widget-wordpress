de601004584c7bf483a422b500b9cbffc06c4990
Riaz <riazlatib@gmail.com>
2019-12-24 13:03:57 -0500

    init setup

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 ## WordPress Sidebar Stats
------------------------
 ###### This is a custom WordPress Plugin that displays within the sidebar 🥑
 
 - Plugin Name: **Sidebar stats widget**
-- Plugin URI:
+- Plugin URI: https://github.com/riazlatib/sidebar-stats-widget-wordpress
 - Description: _Creates a sidebar widget that displays stats about your Wordpress blog including the total number of:_
- - posts
- - authors
- - and comments
+  - posts
+  - authors
+  - and comments
 - Author: **Riaz Latib**
 - Version: 0.12242019
 - Author URI: http://www.riazlatib.com
---
+++ sidebar-stats.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Plugin Name: Sidebar stats widget
+Plugin URI: https://github.com/riazlatib/sidebar-stats-widget-wordpress
+Description: Creates a sidebar widget that displays stats about your Wordpress blog including the total number of posts, authors, and comments.
+Author: Riaz Latib
+Version: 0.12242019
+Author URI: http://www.riazlatib.com
+*/
+
+class sidebar_stats_widget extends WP_Widget {
+
+	function sidebar_stats_widget() {
+		// widget actual processes
+		parent::WP_Widget( /* Base ID */'sidebar_stats_widget', /* Name */'Sidebar stats', array( 'description' => 'Posts, comments and user counts' ) );
+	}
+
+	function form($instance) {
+		// outputs the options form on admin
+
+		// format title for html
+		$title = htmlspecialchars($instance['title'], ENT_QUOTES);
+
+		$beforeStat = $instance['beforeStat'];
+		$afterStat = $instance['afterStat'];
+?>
+	<p>
+	<label for="<?php echo $this->get_field_id('title'); ?>" style="line-height:35px;display:block;">Title: <input type="text" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title; ?>" /></label>
+	<label for="<?php echo $this->get_field_id('beforeStat'); ?>" style="line-height:35px;display:block;">Before each #: <input type="text" id="<?php echo $this->get_field_id('beforeStat'); ?>" name="<?php echo $this->get_field_name('beforeStat'); ?>" value="<?php echo $beforeStat; ?>" /></label>
+	<label for="<?php echo $this->get_field_id('afterStat'); ?>" style="line-height:35px;display:block;">After each #: <input type="text" id="<?php echo $this->get_field_id('afterStat'); ?>" name="<?php echo $this->get_field_name('afterStat'); ?>" value="<?php echo $afterStat; ?>" /></label>
+	</p>
+<?php
+	}
+
+	function update($new_instance, $old_instance) {
+		// processes widget options to be saved
+		$instance = $old_instance;
+		$instance['title'] = strip_tags(stripslashes( $new_instance['title'] ));
+		$instance['beforeStat'] = $new_instance['beforeStat'];
+		$instance['afterStat'] = $new_instance['afterStat'];
+		return $instance;
+	}
+
+	function widget($args, $instance) {
+		// outputs the content of the widget
+		extract($args, EXTR_SKIP);
+
+		$title = empty($instance['title']) ? '&nbsp;' : apply_filters('widget_title', $instance['title']);
+		$beforeStat = $instance['beforeStat'];
+		$afterStat = $instance['afterStat'];
+
+		global $wpdb;
+		$authorCount = count( $wpdb->get_results("SELECT DISTINCT post_author, COUNT(ID) AS count FROM $wpdb->posts WHERE post_type = 'post' AND " . get_private_posts_cap_sql( 'post' ) . " GROUP BY post_author", ARRAY_A));
+		$postCount = $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->posts WHERE post_status = 'publish' AND post_type = 'post'");
+		$commentCount = $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->comments WHERE comment_approved = 1");
+
+		echo $before_widget;
+		if ( !empty( $title ) ) { echo $before_title . $title . $after_title; };
+		echo $beforeStat . $authorCount . $afterStat . " contributors have published<br>";
+		echo $beforeStat . $postCount . $afterStat . " posts that generated<br>";
+		echo $beforeStat . $commentCount . $afterStat . " comments";
+		echo $after_widget;
+	}
+}
+
+if( !function_exists('register_sidebar_stats_widget')){
+	add_action('widgets_init', 'register_sidebar_stats_widget');
+	function register_sidebar_stats_widget() {
+	    register_widget('sidebar_stats_widget');
+	}
+}
+?>
